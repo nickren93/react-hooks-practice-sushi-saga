66e549a3e880d4b9d24cdc762b020779b57dd5bb
Sitong <nickren93@gmail.com>
2025-02-28 22:30:09 -0500

    complete challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [suShiList, setSuShiList] = useState([])
+  const [plates, setPlates] = useState([])
+  const [money, setMoney] = useState(100)
+
+  //const [eaten, setEaten] = useState(false)
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/sushis")
+    .then(resp => resp.json())
+    .then(data => setSuShiList(data))
+  }, [])
+
+  function handleEat(price){
+    if(money >= price){
+      const remainMoney = money - price
+      setMoney(remainMoney)
+      setPlates([...plates, price])
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer suShiList={suShiList} onHandleEat={handleEat} money={money} />
+      <Table plates={plates} money={money}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer( { suShiList, onHandleEat, money }) {
+
+  const [number, setNumber] = useState(0)
+
+  const suShiElement = suShiList.map((suShi) => {
+    return <Sushi key={suShi.id} name={suShi.name} 
+      img={suShi.img_url} price={suShi.price}
+      onHandleEat={onHandleEat} money={money}/>
+  })
+
+  const suShiToShow = suShiElement.slice(number, number+4)
+
+  function handleMore(){
+    const newNumber = number + 4 
+    setNumber(newNumber)
+  }
+
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {suShiToShow}
+      <MoreButton onHandleMore={handleMore} />
     </div>
   );
 }
